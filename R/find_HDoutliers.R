75725f3411f7db58197158ae60f280ad337c6362
priyangt <pritalagala@gmail.com>
2018-01-08 10:40:18 +1100

    update get_outliers using only KNN distance

--- R/find_HDoutliers.R
+++ R/find_HDoutliers.R
@@ -22,8 +22,8 @@
 #' @importFrom HDoutliers getHDmembers
 #' @importFrom FactoMineR MCA
 #' @importFrom gridExtra grid.arrange
-#' @importFrom tibble tibble
 #' @importFrom dplyr bind_rows
+#' @import tibble
 #' @import ggplot2
 #' @import stats
 #' @references {Wilkinson, L. (2016). Visualizing outliers.}
@@ -65,32 +65,32 @@
 #' gridExtra::grid.arrange(data_plot, output_plot , nrow=1 )
 
 find_HDoutliers <- function(data, maxrows = 10000, radius = NULL, alpha = 0.05, l=0.2){
-  # look for categorical variables
-  if (is.null(dim(data))) {
-    CAT <- !is.numeric(data)
-    data <- if (CAT)  as.data.frame(data) else as.matrix(data)
-  } else {
-    if (inherits(data, "matrix")) data <- as.data.frame(data)
+# look for categorical variables
+if (is.null(dim(data))) {
+  CAT <- !is.numeric(data)
+  data <- if (CAT)  tibble::as.tibble(data) else as.matrix(data)
+} else {
+    if (inherits(data, "matrix")) data <- tibble::as.tibble(data)
     CAT <- sapply(data, function(x) !is.numeric(x))
-  }
+}
 
-  if (any(CAT)) {
-    # convert each categorical variable to numeric via
-    # multiple correspondence analysis using MCA function from FactoMineR
-    #  require(FactoMineR)
-    data[,CAT] <- sapply(data[, CAT, drop = F], function(x) FactoMineR::MCA(as.matrix(x), ncp = 1, graph = F)$ind$coord)
-  }
+if (any(CAT)) {
+  # convert each categorical variable to numeric via
+  # multiple correspondence analysis using MCA function from FactoMineR
+  #  require(FactoMineR)
+   data[,CAT] <- sapply(data[, CAT, drop = F], function(x) FactoMineR::MCA(as.matrix(x), ncp = 1, graph = F)$ind$coord)
+}
 
-  unitize <- function(z) {
-    median.z <- stats::median(z)
-    iqr.z <- stats::IQR(z)
-    if (iqr.z == 0) return(rep(0, length(z)))
-    (z - median.z) / iqr.z
-  }
+unitize <- function(z) {
+  zrange <- range(z)
+  if(!(dif<- diff(zrange)))
+  return(rep(0, length(z)))
+  (z-zrange[1])/dif
+}
 
-  udata <- apply(as.matrix(data), 2, unitize)
-  members <- HDoutliers::getHDmembers(udata, radius = radius, maxrows = maxrows)
-  get_outliers(udata, members, alpha = alpha, l = l)
+udata <- apply(as.matrix(data), 2, unitize)
+members <- HDoutliers::getHDmembers(udata, radius = radius, maxrows = maxrows)
+get_outliers(udata, members, alpha = alpha, l = l)
 }
 
 
@@ -113,64 +113,36 @@ find_HDoutliers <- function(data, maxrows = 10000, radius = NULL, alpha = 0.05, 
 #' @export
 #' @importFrom FNN knn.dist
 #' @seealso \code{\link[HDoutliers]{getHDmembers}},  \code{\link{find_HDoutliers}}
-get_outliers <- function(data, memberLists, alpha = 0.05, l=0.2) {
-  exemplars <- sapply(memberLists, function(x) x[[1]])
-  data <- as.matrix(data)
+get_outliers <- function(data, memberLists, alpha = 0.05, l = 0.2) {
 
-  k = ceiling(length(exemplars)[1] / 20)
-  if (k == 1) {
-    d <- FNN::knn.dist(data[exemplars, ], k )
-  } else {
-    d_1 <- FNN::knn.dist(data[exemplars, ], k )
-    d_2 <- apply(d_1, 1, diff)
-    if(is.null(dim(d_2))) {
-      d_3 <- d_2
-    } else {
-      d_3 <- apply(d_2, 2, max)
-    }
-    d <- apply(cbind(d_1[, 1], d_3), 1, max)
+exemplars <- sapply(memberLists, function(x) x[[1]])
+data <- as.matrix(data)
+k <- ceiling(length(exemplars)[1] / 20)
+
+d_knn <- FNN::knn.dist(data[exemplars, ], k )
+d<- apply(d_knn, 1, max)
+
+n <- length(d)
+ord <- order(d)
+gaps <- c(0, diff(d[ord]))
+n4 <- max(min(50, floor(n/4)), 2)
+J <- 1:n4
+start <- max(floor(n/2), 1) + 1
+ghat <- numeric(n)
+for (i in start:n) ghat[i] <- sum((J/n4) * gaps[i - J + 1])
+logAlpha <- log(1/alpha)
+use <- start:n
+bound <- Inf
+for (i in start:n) {
+  if (gaps[i] > logAlpha * ghat[i]) {
+    bound <- d[ord][i - 1]
+    break
   }
-
-  n <- length(d)
-  ord <- order(d)
-  dmin <- min(d)
-  dmax <- max(d)
-  gaps <- c(0, diff(d[ord]))
-  n4 <- max(min(50, floor(n / 4)), 2)
-  J <- 1:(n4 - 1)
-  start <- max(floor(n / 2), 1) + 1
-  K <- n:1
-  std.gaps <- (n4:1) * gaps[(length(gaps) - n4 + 1):length(gaps)]
-  start.gap <-stats::quantile(std.gaps, 0.75)
-  ghat.2 <- start.gap/(n:1) + l*stats::IQR(std.gaps)
-  ghat <- ghat.2
-  alpha2 <- 0.05
-  logAlpha <- log(1 / alpha)
-  start.2 <- max(floor(n * (1 - alpha2)), 1) + 1
-  use <- start.2:n
-  bound <- Inf
-  outlier.condition <- FALSE
-  for (i in start.2:n) {
-    if (gaps[i] > logAlpha * ghat[i]) {
-      bound <- d[ord][i - 1]
-      outlier.condition <- TRUE
-      break
-    }
-  }
-
-  if (outlier.condition) {
-    pending.outlier.index <- i:length(gaps)
-    pending.outlier.vals <- gaps[pending.outlier.index]
-    num.pending.outliers <- length(pending.outlier.index)
-    spacing.index <- pending.outlier.index[which.max(pending.outlier.vals * (num.pending.outliers:1))]:length(gaps)
-    ex1 <- ord[spacing.index]
-    ex <- exemplars[ex1]
-  } else {
-    ex <- exemplars[which(d > bound)]
-  }
-
-  mem1 <- sapply(memberLists, function(x) x[1])
-  out <- unlist(memberLists[match(ex, mem1)])
-  return(out)
+}
+ex <- exemplars[which(d > bound)]
+mem1 <- sapply(memberLists, function(x) x[1])
+out <- unlist(memberLists[match(ex, mem1)])
+names(out) <- NULL
+return(out)
 }
 
