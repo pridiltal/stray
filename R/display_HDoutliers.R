62e50c69ab82980babfcec0b440286e49b39c09c
priyangt <pritalagala@gmail.com>
2018-02-22 10:49:24 +1100

    added display_HDoutliers

---
+++ R/display_HDoutliers.R
@@ -0,0 +1,15 @@
+#' Display outliers with a scatterplot
+#'
+#' Animate a 2D tour path with a scatterplot to visualize outliers.
+#'
+#' @param data A vector, matrix, or data frame consisting of numeric
+#' and/or categorical variables.
+#' @param outliers A vector of indexes of the observations determined to be
+#' outliers by \code{\link[stray]{find_HDoutliers}}
+#' @param col color to be plotted.  Defaults to "black"
+#' @param pch size of the point to be plotted.  Defaults to 20.
+#' @import tourr
+#' @export
+display_HDoutliers <- function(data, outliers, col, pch) {
+print("hi")
+}
