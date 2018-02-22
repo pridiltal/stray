8ad980d0279fc4d42bc80d2bde325cd3ace01ba7
priyangt <pritalagala@gmail.com>
2018-02-22 13:50:20 +1100

    update display_HDoutliers function

--- R/display_HDoutliers.R
+++ R/display_HDoutliers.R
@@ -8,11 +8,21 @@
 #' outliers by \code{\link[stray]{find_HDoutliers}}
 #' @param col color to be plotted.  Defaults to "black"
 #' @param pch size of the point to be plotted.  Defaults to 20.
+#' @importFrom colorspace rainbow_hcl
+#' @importFrom tibble as.tibble
+#' @importFrom dplyr mutate
 #' @import tourr
 #' @export
 display_HDoutliers <- function(data, outliers, col, pch) {
-  d <- ncol(as.matrix(data))
+  data <- tibble::as.tibble(data)
+  d <- ncol(data)
+  outcon <- as.factor(ifelse(1:nrow(data) %in% outliers,
+                             "outlier", "non_outlier"))
+  data <-dplyr::mutate(data, outcon)
   if(d==1) {
+
+    pal <- colorspace::rainbow_hcl(length(levels(data$outcon)))
+    col <- pal[as.numeric(data$outcon)]
     print("one dimensional plot")
   } else if (d==2){
     print("two dimensional plot")
