ed86e05f2106a4e77a2fe2741b823ef42022e2b9
priyangt <pritalagala@gmail.com>
2018-02-23 11:54:08 +1100

    update vignette

--- vignettes/Comparative-Analysis.R
+++ vignettes/Comparative-Analysis.R
@@ -4,10 +4,3 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
-
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
-
